091e93956eb2ee08e01d7b93580c0bd5ceb2f4ce
preyawnshoe <kumar.priyanshu2k02@gmail.com>
2025-05-26 17:28:44 +0530

    graph snippet added

--- main.cpp
+++ main.cpp
@@ -396,7 +396,13 @@ void rd(vc<T> &x) {
 void read() {}
 template <class H, class... T>
 void read(H &h, T &... t) {
-  rd(h), read(t...);
+  cin >> h;
+  read(t...);
+}
+template<typename T>
+void read(std::vector<T> &vec) {
+    for (auto &x : vec)
+        read(x);
 }
  
 void wt(const char c) {
@@ -556,12 +562,324 @@ void yes(bool t = 1) { print(t ? "yes" : "no"); }
 void no(bool t = 1) { yes(!t); }
 void YA(bool t = 1) { print(t ? "YA" : "TIDAK"); }
 void TIDAK(bool t = 1) { YA(!t); }
-#line 3 "main.cpp"
+
+#line 2 "library/ds/hashmap.hpp"
+ 
+// u64 -> Val
+template <typename Val>
+struct HashMap {
+  // n は入れたいものの個数で ok
+  HashMap(u32 n = 0) { build(n); }
+  void build(u32 n) {
+    u32 k = 8;
+    while (k < n * 2) k *= 2;
+    cap = k / 2, mask = k - 1;
+    key.resize(k), val.resize(k), used.assign(k, 0);
+  }
+ 
+  // size を保ったまま. size=0 にするときは build すること.
+  void clear() {
+    used.assign(len(used), 0);
+    cap = (mask + 1) / 2;
+  }
+  int size() { return len(used) / 2 - cap; }
+ 
+  int index(const u64& k) {
+    int i = 0;
+    for (i = hash(k); used[i] && key[i] != k; i = (i + 1) & mask) {}
+    return i;
+  }
+ 
+  Val& operator[](const u64& k) {
+    if (cap == 0) extend();
+    int i = index(k);
+    if (!used[i]) { used[i] = 1, key[i] = k, val[i] = Val{}, --cap; }
+    return val[i];
+  }
+ 
+  Val get(const u64& k, Val default_value) {
+    int i = index(k);
+    return (used[i] ? val[i] : default_value);
+  }
+ 
+  bool count(const u64& k) {
+    int i = index(k);
+    return used[i] && key[i] == k;
+  }
+ 
+  // f(key, val)
+  template <typename F>
+  void enumerate_all(F f) {
+    FOR(i, len(used)) if (used[i]) f(key[i], val[i]);
+  }
+ 
+private:
+  u32 cap, mask;
+  vc<u64> key;
+  vc<Val> val;
+  vc<bool> used;
+ 
+  u64 hash(u64 x) {
+    static const u64 FIXED_RANDOM = std::chrono::steady_clock::now().time_since_epoch().count();
+    x += FIXED_RANDOM;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return (x ^ (x >> 31)) & mask;
+  }
+ 
+  void extend() {
+    vc<pair<u64, Val>> dat;
+    dat.reserve(len(used) / 2 - cap);
+    FOR(i, len(used)) {
+      if (used[i]) dat.eb(key[i], val[i]);
+    }
+    build(2 * len(dat));
+    for (auto& [a, b]: dat) (*this)[a] = b;
+  }
+};
+#line 3 "library/graph/base.hpp"
+template <typename T>
+struct Edge {
+  int frm, to;
+  T cost;
+  int id;
+};
+ 
+template <typename T = int, bool directed = false>
+struct Graph {
+  static constexpr bool is_directed = directed;
+  int N, M;
+  using cost_type = T;
+  using edge_type = Edge<T>;
+  vector<edge_type> edges;
+  vector<int> indptr;
+  vector<edge_type> csr_edges;
+  vc<int> vc_deg, vc_indeg, vc_outdeg;
+  bool prepared;
+ 
+  class OutgoingEdges {
+  public:
+    OutgoingEdges(const Graph* G, int l, int r) : G(G), l(l), r(r) {}
+ 
+    const edge_type* begin() const {
+      if (l == r) { return 0; }
+      return &G->csr_edges[l];
+    }
+ 
+    const edge_type* end() const {
+      if (l == r) { return 0; }
+      return &G->csr_edges[r];
+    }
+ 
+  private:
+    const Graph* G;
+    int l, r;
+  };
+ 
+  bool is_prepared() { return prepared; }
+ 
+  Graph() : N(0), M(0), prepared(0) {}
+  Graph(int N) : N(N), M(0), prepared(0) {}
+ 
+  void build(int n) {
+    N = n, M = 0;
+    prepared = 0;
+    edges.clear();
+    indptr.clear();
+    csr_edges.clear();
+    vc_deg.clear();
+    vc_indeg.clear();
+    vc_outdeg.clear();
+  }
+ 
+  void add(int frm, int to, T cost = 1, int i = -1) {
+    assert(!prepared);
+    assert(0 <= frm && 0 <= to && to < N);
+    if (i == -1) i = M;
+    auto e = edge_type({frm, to, cost, i});
+    edges.eb(e);
+    ++M;
+  }
+ 
+#ifdef FASTIO
+  // wt, off
+  void read_tree(bool wt = false, int off = 1) { read_graph(N - 1, wt, off); }
+ 
+  void read_graph(int M, bool wt = false, int off = 1) {
+    for (int m = 0; m < M; ++m) {
+      INT(a, b);
+      a -= off, b -= off;
+      if (!wt) {
+        add(a, b);
+      } else {
+        T c;
+        read(c);
+        add(a, b, c);
+      }
+    }
+    build();
+  }
+#endif
+ 
+  void build() {
+    assert(!prepared);
+    prepared = true;
+    indptr.assign(N + 1, 0);
+    for (auto&& e: edges) {
+      indptr[e.frm + 1]++;
+      if (!directed) indptr[e.to + 1]++;
+    }
+    for (int v = 0; v < N; ++v) { indptr[v + 1] += indptr[v]; }
+    auto counter = indptr;
+    csr_edges.resize(indptr.back() + 1);
+    for (auto&& e: edges) {
+      csr_edges[counter[e.frm]++] = e;
+      if (!directed) csr_edges[counter[e.to]++] = edge_type({e.to, e.frm, e.cost, e.id});
+    }
+  }
+ 
+  OutgoingEdges operator[](int v) const {
+    assert(prepared);
+    return {this, indptr[v], indptr[v + 1]};
+  }
+ 
+  vc<int> deg_array() {
+    if (vc_deg.empty()) calc_deg();
+    return vc_deg;
+  }
+ 
+  pair<vc<int>, vc<int>> deg_array_inout() {
+    if (vc_indeg.empty()) calc_deg_inout();
+    return {vc_indeg, vc_outdeg};
+  }
+ 
+  int deg(int v) {
+    if (vc_deg.empty()) calc_deg();
+    return vc_deg[v];
+  }
+ 
+  int in_deg(int v) {
+    if (vc_indeg.empty()) calc_deg_inout();
+    return vc_indeg[v];
+  }
+ 
+  int out_deg(int v) {
+    if (vc_outdeg.empty()) calc_deg_inout();
+    return vc_outdeg[v];
+  }
+ 
+#ifdef FASTIO
+  void debug() {
+#ifdef LOCAL
+    print("Graph");
+    if (!prepared) {
+      print("frm to cost id");
+      for (auto&& e: edges) print(e.frm, e.to, e.cost, e.id);
+    } else {
+      print("indptr", indptr);
+      print("frm to cost id");
+      FOR(v, N) for (auto&& e: (*this)[v]) print(e.frm, e.to, e.cost, e.id);
+    }
+#endif
+  }
+#endif
+ 
+  vc<int> new_idx;
+  vc<bool> used_e;
+ 
+  // G における頂点 V[i] が、新しいグラフで i になるようにする
+  // {G, es}
+  // sum(deg(v)) の計算量になっていて、
+  // 新しいグラフの n+m より大きい可能性があるので注意
+  Graph<T, directed> rearrange(vc<int> V, bool keep_eid = 0) {
+    if (len(new_idx) != N) new_idx.assign(N, -1);
+    int n = len(V);
+    FOR(i, n) new_idx[V[i]] = i;
+    Graph<T, directed> G(n);
+    vc<int> history;
+    FOR(i, n) {
+      for (auto&& e: (*this)[V[i]]) {
+        if (len(used_e) <= e.id) used_e.resize(e.id + 1);
+        if (used_e[e.id]) continue;
+        int a = e.frm, b = e.to;
+        if (new_idx[a] != -1 && new_idx[b] != -1) {
+          history.eb(e.id);
+          used_e[e.id] = 1;
+          int eid = (keep_eid ? e.id : -1);
+          G.add(new_idx[a], new_idx[b], e.cost, eid);
+        }
+      }
+    }
+    FOR(i, n) new_idx[V[i]] = -1;
+    for (auto&& eid: history) used_e[eid] = 0;
+    G.build();
+    return G;
+  }
+ 
+  Graph<T, true> to_directed_tree(int root = -1) {
+    if (root == -1) root = 0;
+    assert(!is_directed && prepared && M == N - 1);
+    Graph<T, true> G1(N);
+    vc<int> par(N, -1);
+    auto dfs = [&](auto& dfs, int v) -> void {
+      for (auto& e: (*this)[v]) {
+        if (e.to == par[v]) continue;
+        par[e.to] = v, dfs(dfs, e.to);
+      }
+    };
+    dfs(dfs, root);
+    for (auto& e: edges) {
+      int a = e.frm, b = e.to;
+      if (par[a] == b) swap(a, b);
+      assert(par[b] == a);
+      G1.add(a, b, e.cost);
+    }
+    G1.build();
+    return G1;
+  }
+ 
+  HashMap<int> MP_FOR_EID;
+ 
+  int get_eid(u64 a, u64 b) {
+    if (len(MP_FOR_EID) == 0) {
+      MP_FOR_EID.build(N - 1);
+      for (auto& e: edges) {
+        u64 a = e.frm, b = e.to;
+        u64 k = to_eid_key(a, b);
+        MP_FOR_EID[k] = e.id;
+      }
+    }
+    return MP_FOR_EID.get(to_eid_key(a, b), -1);
+  }
+ 
+  u64 to_eid_key(u64 a, u64 b) {
+    if (!directed && a > b) swap(a, b);
+    return N * a + b;
+  }
+ 
+private:
+  void calc_deg() {
+    assert(vc_deg.empty());
+    vc_deg.resize(N);
+    for (auto&& e: edges) vc_deg[e.frm]++, vc_deg[e.to]++;
+  }
+ 
+  void calc_deg_inout() {
+    assert(vc_indeg.empty());
+    vc_indeg.resize(N);
+    vc_outdeg.resize(N);
+    for (auto&& e: edges) { vc_indeg[e.to]++, vc_outdeg[e.frm]++; }
+  }
+};
+
+#line 4 "main.cpp"
  
 void solve() {
+
 }
- 
-signed main() {
+
+ int main() {
   INT(T);
   FOR(T) solve();
+  return 0;
 }
